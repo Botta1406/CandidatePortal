4bccc9753054096477d7f442dcf1303268a7bc9f
Botta1406 <bottahema2002@gmail.com>
2025-04-24 15:43:58 +0530

    Update Navbar with Home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-// components/Navbar.tsx
+// // components/Navbar.tsx
+
 import Link from 'next/link';
 
 export default function Navbar() {
     return (
         <nav className="bg-gray-800 p-4 text-white flex gap-4">
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/dashboard/jobs">Jobs</Link>
-            <Link href="/dashboard/applied">Applied</Link>
+            <Link href="/" className="hover:underline">Home</Link> {/* Home link */}
+            <Link href="/dashboard" className="hover:underline">Dashboard</Link>
+            <Link href="/dashboard/jobs" className="hover:underline">Jobs</Link>
+            <Link href="/dashboard/applied" className="hover:underline">Applied</Link>
         </nav>
     );
 }
